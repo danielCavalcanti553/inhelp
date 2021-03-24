d37215c218da6f57e0cb981135a1894e02e80bab
danielsouza553@gmail.com <danielsouza553@gmail.com>
2021-03-24 15:01:27 -0300

    pesquisar-profissionais

--- ionic-proj/inhelp/src/app/app-routing.module.ts
+++ ionic-proj/inhelp/src/app/app-routing.module.ts
@@ -18,6 +18,10 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'pesquisar-profissionais',
+    loadChildren: () => import('./pesquisar-profissionais/pesquisar-profissionais.module').then( m => m.PesquisarProfissionaisPageModule)
   }
 ];
 
